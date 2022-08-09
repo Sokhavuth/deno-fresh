3cf6ffad2bb5e5c5f92b4935b30a34a74220d272
Sokhavuth <vuthdevelop@gmail.com>
2022-08-09 11:20:35 +0700

    Make it better

--- setting.js
+++ setting.js
@@ -10,4 +10,24 @@ function setting(){
     return configure
 }
 
-export { setting }
+
+import { config } from "config";
+await config({export: true});
+const secret_key = Deno.env.get("SECRET_KEY");
+ 
+ 
+import { MongoClient } from "mongodb";
+const client = await new MongoClient();
+await client.connect(Deno.env.get('DATABASE_URI'));
+const mymongo = client.database(Deno.env.get('DB_NAME'));
+ 
+ 
+import { connect } from "redis"
+const myredis = await connect({
+    hostname: Deno.env.get('REDIS_URI'),
+    port: parseInt(Deno.env.get('REDIS_PORT')),
+    password: Deno.env.get('REDIS_PASSWORD'),
+});
+
+
+export { setting, secret_key, mymongo, myredis }
